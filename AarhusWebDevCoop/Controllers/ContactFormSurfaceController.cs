644a7eb3f2ee659f1856440f0d8234dee88279dc
KarinaTimea <szocs.karina@gmail.com>
2016-03-04 09:36:37 +0100

    first commit

---
+++ AarhusWebDevCoop/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,28 @@
+﻿using AarhusWebDevCoop.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace AarhusWebDevCoop.Controllers
+{
+    public class ContactFormSurfaceController : Umbraco.Web.Mvc.SurfaceController
+
+    {
+        // GET: ContactFormSurface
+        public ActionResult Index()
+        {
+            return PartialView("ContactForm", new ContactFormSurfaceController());
+        }
+
+        [HttpPost]
+        public ActionResult HandleFormSubmit(ContactForm model)
+        {
+            if (!ModelState.IsValid) { return CurrentUmbracoPage(); } // Read data from model and send mail 
+            TempData["success"] = true;
+            return RedirectToCurrentUmbracoPage(); }
+
+        }
+    }
