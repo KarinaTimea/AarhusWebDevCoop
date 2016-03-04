d62b558b0aa7a26b282000996e7a4c398fa75d16
KarinaTimea <szocs.karina@gmail.com>
2016-03-04 13:54:08 +0100

    Form working

--- AarhusWebDevCoop/Controllers/ContactFormSurfaceController.cs
+++ AarhusWebDevCoop/Controllers/ContactFormSurfaceController.cs
@@ -4,7 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Umbraco.Web.Mvc;
+using System.Net.Mail;
+
 
 namespace AarhusWebDevCoop.Controllers
 {
@@ -14,15 +15,36 @@ namespace AarhusWebDevCoop.Controllers
         // GET: ContactFormSurface
         public ActionResult Index()
         {
-            return PartialView("ContactForm", new ContactFormSurfaceController());
+            return PartialView("ContactForm", new ContactForm());
         }
 
         [HttpPost]
         public ActionResult HandleFormSubmit(ContactForm model)
         {
-            if (!ModelState.IsValid) { return CurrentUmbracoPage(); } // Read data from model and send mail 
-            TempData["success"] = true;
-            return RedirectToCurrentUmbracoPage(); }
+            if (!ModelState.IsValid) { return CurrentUmbracoPage(); }
 
-        }
+            MailMessage message = new MailMessage();
+            message.To.Add("szocs.karina@gmail.com");
+            message.Subject = model.Subject;
+            message.From = new MailAddress(model.Email, model.Name);
+            message.Body = model.Message;
+
+            using (SmtpClient smtp = new SmtpClient())
+            {
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.UseDefaultCredentials = false;
+                //smtp.EnableSsl = true;
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.Credentials = new System.Net.NetworkCredential("szocs.karina@gmail.com", "hlmuxkpuulesrwwd");
+                smtp.EnableSsl = true;
+                // send mail
+                smtp.Send(message);
+            }
+
+            TempData["success"] = true;
+
+            return RedirectToCurrentUmbracoPage();
+        } 
     }
+}
